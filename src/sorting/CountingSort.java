92d0adf59e721fb0901e447282106ce95762ab3c
vudinhlinh20194606 <linh.vd194606@sis.hust.edu.vn>
2023-12-27 17:15:14 +0700

    linh

---
+++ src/sorting/CountingSort.java
@@ -0,0 +1,36 @@
+package sorting;
+
+import utils.ArrayUtils;
+
+public class CountingSort extends SortingAlgorithms {
+	public CountingSort(int[] array) {
+		super(array);
+	}
+	
+	@Override
+	public void sort() {
+		int[] counts = counter();
+		
+		int numRestored = 0;
+		for(int i = 0; i < counts.length; i++) {
+			while(counts[i] > 0 & numRestored < inputArray.length) {
+				inputArray[numRestored] = i;
+				counts[i]--;
+				addLogs(inputArray, counts, new int[] {numRestored, i}, "Restored element" + i +", and decrease relevant counter by 1");
+				numRestored++;
+			}
+		}
+	}
+
+	private int[] counter() {
+		int max = ArrayUtils.max(inputArray);
+		int[] counts = new int[max+1];
+		addLogs(inputArray, counts, new int[] {-1,-1}, "Create key counting array" + "(" + 0 + " to " + max + ")");
+		
+		for(int i = 0; i < inputArray.length; i++) {
+			counts[inputArray[i]] += 1;
+			addLogs(inputArray, counts, new int[] {i, inputArray[i]}, "Increase the counter with key " + inputArray[i] + " by 1");
+		}
+		return counts;
+	}
+}
